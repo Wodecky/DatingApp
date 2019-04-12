91df081fbf393f27a287021456aa4cca6ca0f214
Maciej <maciej.krebs@gmail.com>
2019-04-12 18:22:37 +0200

    Zmiana kompa

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -8,8 +8,10 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { MemberEditResolver } from './_resolvers/member-edit.resolver.';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
@@ -19,8 +21,8 @@ export const appRoutes: Routes = [
             { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
             { path: 'member/edit', component: MemberEditComponent,  resolve: {user: MemberEditResolver},
                  canDeactivate: [PreventUnsavedChanges]},
-            { path: 'messages', component: MessagesComponent},
-            { path: 'lists', component: ListsComponent},
+            { path: 'messages', component: MessagesComponent, resolve: {messages: MessagesResolver}},
+            { path: 'lists', component: ListsComponent, resolve: {users: ListsResolver}},
         ] },
     { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
